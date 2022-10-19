44e39ca380394a6fe329875f1583075e23508c37
ptlmeet27 <ptlmeet2783@gmail.com>
2022-10-20 01:58:18 +0530

    basic setup

---
+++ src/components/display.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Maincont extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <h1>hello</h1>
+        );
+    };
+}
+
+
+export { Maincont} ;
